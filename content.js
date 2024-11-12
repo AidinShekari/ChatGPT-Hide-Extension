b74ab8db7a7be527a8844af0bc582db4345673c9
Aidin Shekari <aidinshkari@gmail.com>
2024-11-12 20:21:59 +0330

    Add files via upload

---
+++ content.js
@@ -0,0 +1,3 @@
+const style = document.createElement('style');
+style.innerHTML = '.flex.flex-col.gap-2.text-token-text-primary.text-sm.false.mt-5.pb-2 { display: none !important; }';
+document.head.appendChild(style);
